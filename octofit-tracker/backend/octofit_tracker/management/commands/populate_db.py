b6806cf4bc39af3e052a2bafa5ebf134c1c4852b
Meghana Gnana Sape <84613899+Meghana-Prasad@users.noreply.github.com>
2025-05-27 09:01:24 +0000

    verify

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -11,26 +11,29 @@ class Command(BaseCommand):
 
         # Populate Users
         for user_data in data['users']:
-            User.objects.create(email=user_data['email'], name=user_data['name'], age=user_data['age'])
+            User.objects.get_or_create(email=user_data['email'], defaults={
+                'name': user_data['name'],
+                'age': user_data['age']
+            })
 
         # Populate Teams
         for team_data in data['teams']:
-            team = Team.objects.create(name=team_data['name'])
+            team, _ = Team.objects.get_or_create(name=team_data['name'])
             members = User.objects.filter(email__in=team_data['members'])
             team.members.set(members)
 
         # Populate Activities
         for activity_data in data['activities']:
             user = User.objects.get(email=activity_data['user'])
-            Activity.objects.create(user=user, activity_type=activity_data['type'], duration=activity_data['duration'])
+            Activity.objects.get_or_create(user=user, activity_type=activity_data['activity_type'], duration=activity_data['duration'])
 
         # Populate Leaderboard
         for leaderboard_data in data['leaderboard']:
             user = User.objects.get(email=leaderboard_data['user'])
-            Leaderboard.objects.create(user=user, score=leaderboard_data['score'])
+            Leaderboard.objects.get_or_create(user=user, score=leaderboard_data['score'])
 
         # Populate Workouts
         for workout_data in data['workouts']:
-            Workout.objects.create(name=workout_data['name'], description=workout_data['description'])
+            Workout.objects.get_or_create(name=workout_data['name'], description=workout_data['description'])
 
         self.stdout.write(self.style.SUCCESS('Database populated successfully using Django ORM!'))
