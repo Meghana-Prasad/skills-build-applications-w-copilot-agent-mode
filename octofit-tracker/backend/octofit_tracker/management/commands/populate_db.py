3c0259b9bbbef9b58975faa78dd13df02dc32835
Meghana Gnana Sape <84613899+Meghana-Prasad@users.noreply.github.com>
2025-05-27 07:23:46 +0000

    populate_db.py

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -11,31 +11,27 @@ class Command(BaseCommand):
 
         # Populate Users
         for user_data in data['users']:
-            user = User(email=user_data['email'], name=user_data['name'], age=user_data['age'])
-            user.save()
+            User.objects.create(email=user_data['email'], name=user_data['name'], age=user_data['age'])
 
         # Populate Teams
         for team_data in data['teams']:
-            team = Team(name=team_data['name'])
-            team.save()
+            team = Team.objects.create(name=team_data['name'])
             members = User.objects.filter(email__in=team_data['members'])
-            team.members.set(members)
+            for member in members:
+                team.members.add(member)
 
         # Populate Activities
         for activity_data in data['activities']:
             user = User.objects.get(email=activity_data['user'])
-            activity = Activity(user=user, activity_type=activity_data['type'], duration=activity_data['duration'])
-            activity.save()
+            Activity.objects.create(user=user, activity_type=activity_data['type'], duration=activity_data['duration'])
 
         # Populate Leaderboard
         for leaderboard_data in data['leaderboard']:
             user = User.objects.get(email=leaderboard_data['user'])
-            leaderboard = Leaderboard(user=user, score=leaderboard_data['score'])
-            leaderboard.save()
+            Leaderboard.objects.create(user=user, score=leaderboard_data['score'])
 
         # Populate Workouts
         for workout_data in data['workouts']:
-            workout = Workout(name=workout_data['name'], description=workout_data['description'])
-            workout.save()
+            Workout.objects.create(name=workout_data['name'], description=workout_data['description'])
 
         self.stdout.write(self.style.SUCCESS('Database populated successfully using Django ORM!'))
